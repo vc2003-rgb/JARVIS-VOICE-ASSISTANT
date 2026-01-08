075b0453a89e18d48d28d9224e52220e58114be1
vc2003-rgb <vc568423@gmail.com>
2026-01-08 12:18:56 +0530

    Add files via upload

---
+++ musicLibrary (1).py
@@ -0,0 +1,6 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&list=RDDeumyOzKqgI&start_radio=1",
+    "wolf": "https://www.youtube.com/watch?v=cH4E_t3m3xM&list=RDcH4E_t3m3xM&start_radio=1–––"
+}
